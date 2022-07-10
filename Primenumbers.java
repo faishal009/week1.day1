d0d56223a5be5450165b3468620cdbbb382e3728
faishal009 <109009397+faishal009@users.noreply.github.com>
2022-07-10 08:41:29 +0530

    Add files via upload

---
+++ Primenumbers.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Primenumbers {
+
+	public static void main(String[] args) {
+		int a=12;
+		if (a%2==0) {
+		System.out.println("The number is prime number");}
+	else {
+			System.out.println("The number is even number");
+			
+}
+
+}
+}
